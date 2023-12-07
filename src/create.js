ddfe0da5dbcf9c4b174cb00fcb63256f76b78a01
pythonPlant12 <pythonplant12@gmail.com>
2023-12-07 19:45:56 +0100

    Practise 1

--- src/create.js
+++ src/create.js
@@ -1,9 +1,17 @@
 import { displayLog } from './utils';
+import { Observable } from 'rxjs';
 
 export default () => {
     /** start coding */
+    // Así como creo nuevo Observable
+    const myObservable = new Observable(function(observer) {
+        observer.next("Hello") // con .next() hago push de valores, desde el observable al observer
+        setTimeout(() => {
+            observer.next(" world.")
+        }, 2000);
+    })
 
 
-
+    const subscribeToMyObservable = myObservable.subscribe(evento => displayLog(evento))
     /** end coding */
 }
