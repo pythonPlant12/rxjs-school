d2cecd2ec9b8ce6ac0951bf99d92072f9f264c7f
pythonPlant12 <pythonplant12@gmail.com>
2023-12-07 20:01:21 +0100

    Observable from (secuencia de flujo de datos)

--- src/create.js
+++ src/create.js
@@ -1,7 +1,18 @@
-import { displayLog } from './utils';
+import { displayLog } from "./utils";
+import { from } from "rxjs";
 
 export default () => {
-    /** start coding */
-
-    /** end coding */
-}
+	/** start coding */
+	const myArray = [1, 2, 3, 4, 5];
+	const myPromise = new Promise((resolve) =>
+		setTimeout(() => {
+			resolve("Hello World");
+		}, 2000)
+	);
+	const observable = from(myArray);
+	const observable1 = from(myPromise);
+	// val => displayLog(val) es una versión reducida del observer
+	const subscription = observable.subscribe((val) => displayLog(val));
+	observable1.subscribe((val) => displayLog(val));
+	/** end coding */
+};
