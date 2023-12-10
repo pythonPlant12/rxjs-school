7d4d5015c7c06aa5ce9d4ed48fcf4327ae1e8189
pythonPlant12 <pythonplant12@gmail.com>
2023-12-10 19:45:59 +0100

    fromEvent observable

--- src/create.js
+++ src/create.js
@@ -1,9 +1,19 @@
 import { displayLog } from './utils';
+import { fromEvent } from 'rxjs';
 
 export default () => {
     /** start coding */
+    const actionBtn = document.getElementById('action-btn')
 
+    // Creo observable fromEvent, pasandole el elemento del botón y la acción que quiero escuchar
+    const source = fromEvent(actionBtn, 'click')
+    source.subscribe(datos => {
+        displayLog(`click event at pos (${datos.x}, ${datos.y})`)
+    })
 
-
+    fromEvent(document, 'mousemove').subscribe(evento => {
+            console.log(evento)
+        }
+    )
     /** end coding */
 }
